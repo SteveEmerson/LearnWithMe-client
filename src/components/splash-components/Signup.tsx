bc85be4105a0e56f0018a74fb5f9e2a46f140ae4
Steve Emerson <sbemerson@outlook.com>
2021-04-08 07:42:03 -0400

    cleanup

--- src/components/splash-components/Signup.tsx
+++ src/components/splash-components/Signup.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useForm } from 'react-hook-form';
 
 type SignupProps = {
   setAppState: Function
@@ -15,6 +16,12 @@ type SignupState = {
   user: User
 }
 
+type FormData = {
+  name: string;
+  email: string;
+  password: string;
+}
+
 class Signup extends React.Component<SignupProps, SignupState> {
   constructor(props: SignupProps){
     super(props);
@@ -28,11 +35,12 @@ class Signup extends React.Component<SignupProps, SignupState> {
     }
   }
 
+ const { register, handleSubmit } = useForm<FormData>();
+
   render() {
     return(
       <div>
-
-
+        Got to signup
       </div>
     )
   }
