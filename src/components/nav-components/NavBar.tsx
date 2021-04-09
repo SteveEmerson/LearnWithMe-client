1418bc349a9d6fca8616a06316a76bd7a8988522
Steve Emerson <sbemerson@outlook.com>
2021-04-08 20:35:43 -0400

    boilerplate for update settings and navbar

---
+++ src/components/nav-components/NavBar.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+class NavBar extends React.Component<{},{}>{
+  
+  render() {
+    return(
+      <div>
+        This is the navbar.
+      </div>
+    )
+  }
+}
+
+export default NavBar
---
+++ src/components/settings-components/UpdateSettings.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class UpdateSettings extends React.Component<{}, {}>{
+
+  render(){
+
+    return(
+      <div>
+        Got to update settings
+      </div>
+    )
+  }
+}
+
+export default UpdateSettings
