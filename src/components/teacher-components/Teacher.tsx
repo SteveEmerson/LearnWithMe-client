392ea3d9287ebb788af23ff602322e3f5d307217
Steve Emerson <sbemerson@outlook.com>
2021-04-13 21:36:26 -0400

    cleanup

--- src/components/teacher-components/Teacher.tsx
+++ src/components/teacher-components/Teacher.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link, Redirect } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import UpdateSettings from '../settings-components/UpdateSettings'
 import TeacherStudentView from './TeacherStudentView';
 
