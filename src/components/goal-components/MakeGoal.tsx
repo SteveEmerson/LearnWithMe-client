c8dcc8915bd33a5b399901a1f05d84bff7ae19ea
Steve Emerson <sbemerson@outlook.com>
2021-04-16 04:36:07 -0400

    started MakeGoal

--- src/components/goal-components/MakeGoal.tsx
+++ src/components/goal-components/MakeGoal.tsx
@@ -1,6 +1,50 @@
 import * as React from 'react';
 
-class MakeGoal extends React.Component<{},{}>{
+type Student = {
+  id: number
+  displayName: string
+  email: string
+  availability: {}
+  meetings?:Array<Meeting>
+  goal?:Goal
+}
+
+type User = {
+  email: string
+  userId: number
+  displayName: string
+  partnerList: number[]
+  role: string
+  availability: {}
+  sessionToken: string
+}
+
+type Meeting= {
+  id: number,
+  d_t: Date,
+  teacherId: number,
+  studentId: number,
+  createdAt: Date,
+  updatedAt: Date
+}
+
+type Goal = {
+  id: number
+  description: string
+  targetDate: Date
+  createdAt: Date
+  updatedAt: Date
+  studentId: number
+  teacherId: number | null
+}
+
+type MGProps = {
+  teacher: User
+  student: Student
+  setTSVState: Function
+}
+
+class MakeGoal extends React.Component<MGProps,{}>{
   render(){
     return(
       <div>
