ce997842ba25ad9213185578341cd302b97af2ac
Steve Emerson <sbemerson@outlook.com>
2021-04-30 04:03:47 -0400

    Style Splash

--- src/components/splash-components/Home.tsx
+++ src/components/splash-components/Home.tsx
@@ -2,11 +2,11 @@ import * as React from 'react';
 
 function Home() {
   return(
-    <div className="text-center py-40">
-      <h1 className="text-8xl text-blue-500 font-extrabold ">LearnWithMe</h1>
-      <h2 className="text-4xl font-extrabold "> Making Connections Makes Learning</h2>
-      <h4 className="text-2xl font-bold "> Students and teachers working together. </h4>
-      <h4 className="text-2xl font-bold "> Schedule meetups. Set goals. Build success. </h4>
+    <div className="text-center py-28">
+      <p className="text-8xl text-blue-500 font-extrabold mb-10">LearnWithMe</p>
+      <p className="text-3xl font-extrabold "> Making Connections Makes Learning</p>
+      <p className="text-2xl font-bold "> Students and teachers working together. </p>
+      <p className="text-2xl font-bold "> Schedule meetups. Set goals. Build success. </p>
     </div>
   )
 }
