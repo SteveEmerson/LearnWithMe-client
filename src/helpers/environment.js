563983a41503790c64ce748fd4264f2fc05b9022
Steve Emerson <sbemerson@outlook.com>
2021-05-04 20:49:53 -0400

    change to environment

--- src/helpers/environment.js
+++ src/helpers/environment.js
@@ -4,8 +4,9 @@ switch (window.location.hostname) {
   case 'localhost' || '127.0.0.1':
     APIURL = 'http://localhost:3000';
     break;
-  case 'learn-with-me-client.herokuapp.com/':
-    APIURL = 'https://sbe-learn-with-me.herokuapp.com/'
+  case 'learn-with-me-client.herokuapp.com':
+    APIURL = 'https://sbe-learn-with-me.herokuapp.com'
+    break;
 }
 
 export default APIURL;
