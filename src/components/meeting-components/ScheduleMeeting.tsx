cddaa6ed4e89506460a011b15ac703f80bb4272c
Steve Emerson <sbemerson@outlook.com>
2021-05-08 08:13:36 -0400

    Separate date and time on meeting form

--- src/components/meeting-components/ScheduleMeeting.tsx
+++ src/components/meeting-components/ScheduleMeeting.tsx
@@ -105,7 +105,7 @@ class ScheduleMeeting extends React.Component<SMProps,SMState>{
   }
 
   getDateString = (d: Date): string => {
-    return d.toISOString().slice(0,10) + "T07:00"
+    return d.toISOString().slice(0,10)
   }
 
   getMaxDate = (): Date => {
@@ -128,44 +128,52 @@ class ScheduleMeeting extends React.Component<SMProps,SMState>{
   }
 
   handleDate = (e: React.FormEvent<HTMLInputElement>) => {
-    this.setState({d_t: new Date(e.currentTarget.value)})
+    let selectedDate = e.currentTarget.value;
+    let selectedTime = "T07:15:00"
+    let selectedDT = selectedDate + selectedTime
+    //console.log(new Date(selectedDT))
+    this.setState({d_t: new Date(selectedDT)})
   }
 
   handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    let role = this.props.teacher ? 'teacher' : 'student'
     e.preventDefault();
-    const url: string = `${APIURL}/meeting/${role}_create`
-    fetch(url,
-      {
-          method: 'POST',
-          body: JSON.stringify(
-            {
-              d_t: this.state.d_t, 
-              teacherId: this.props.teacher ? this.props.teacher.id: this.state.teacher.id,
-              studentId: this.props.student ? this.props.student.id: this.state.student.id
-            }
-          ),
-          headers: new Headers ({
-          'Content-Type': 'application/json',
-          'Authorization': this.props.token
-          })
-      })
-      .then((res) => res.json())
-      .then((data: Meeting) => {
-
-        if(role === "teacher" && this.props.getTeacherMeetings){
-          this.props.getTeacherMeetings()
-        }
-
-        if(role === "student" && this.props.getStudentMeetings){
-          this.props.getStudentMeetings()
-        }
-
-        this.props.toggleScheduleMeeting()
-      })
-      .catch(err => {
-        console.log(`Error in fetch: ${err}`)
-      }) 
+    let role = this.props.teacher ? 'teacher' : 'student'
+    if(this.state.student.id === 0){
+      window.alert(`You must select a ${role==="teacher" ? "student." : " teacher."}`)
+    }else{        
+      const url: string = `${APIURL}/meeting/${role}_create`
+      fetch(url,
+        {
+            method: 'POST',
+            body: JSON.stringify(
+              {
+                d_t: this.state.d_t, 
+                teacherId: this.props.teacher ? this.props.teacher.id: this.state.teacher.id,
+                studentId: this.props.student ? this.props.student.id: this.state.student.id
+              }
+            ),
+            headers: new Headers ({
+            'Content-Type': 'application/json',
+            'Authorization': this.props.token
+            })
+        })
+        .then((res) => res.json())
+        .then((data: Meeting) => {
+  
+          if(role === "teacher" && this.props.getTeacherMeetings){
+            this.props.getTeacherMeetings()
+          }
+  
+          if(role === "student" && this.props.getStudentMeetings){
+            this.props.getStudentMeetings()
+          }
+  
+          this.props.toggleScheduleMeeting()
+        })
+        .catch(err => {
+          console.log(`Error in fetch: ${err}`)
+        }) 
+    }
   }
 
   componentDidUpdate(){
@@ -174,51 +182,57 @@ class ScheduleMeeting extends React.Component<SMProps,SMState>{
 
   renderTeacherSelect = () => {
     return(
-      <select 
-        name="teachers" 
-        id='teacher-select'
-        onChange={(e) => this.handleTeacherSelect(e.currentTarget.value)}
-      >
-        <option defaultValue="Select">Select a teacher</option>
-        {this.props.allTeachers?.map((teacher: Teacher) => {
-          return(
-            <option 
-              value={teacher.id}
-              key={`TOP${teacher.id}`}
-              disabled = {this.props.student 
-                ? !teacher.partners.includes(this.props.student.id)
-                : undefined}
-              >
-                {teacher.displayName}
-            </option>
-          )
-        })}
-      </select>
+      <div>
+        <label className="font-semibold mr-2" htmlFor="students">Student</label>
+        <select 
+          name="teachers" 
+          id='teacher-select'
+          onChange={(e) => this.handleTeacherSelect(e.currentTarget.value)}
+        >
+          <option defaultValue="Select">--make a selection--</option>
+          {this.props.allTeachers?.map((teacher: Teacher) => {
+            return(
+              <option 
+                value={teacher.id}
+                key={`TOP${teacher.id}`}
+                disabled = {this.props.student 
+                  ? !teacher.partners.includes(this.props.student.id)
+                  : undefined}
+                >
+                  {teacher.displayName}
+              </option>
+            )
+          })}
+        </select>
+      </div>
     )
   }
 
   renderStudentSelect = () => {
     return(
-      <select
-        name="students" 
-        id='student-select'
-        onChange={(e) => this.handleStudentSelect(e.currentTarget.value)}
-      >
-        <option defaultValue="Select">Select a student</option>
-        {this.props.allStudents?.map((student: Student) => {
-          return(
-            <option 
-              value={student.id}
-              key={`SOP${student.id}`}
-              disabled = {this.props.teacher
-                ? !student.partners.includes(this.props.teacher.id)
-                : undefined}
-            >
-              {student.displayName}
-            </option>
-          )
-        })}
-      </select>
+      <div>
+        <label className="font-semibold mr-2" htmlFor="students">Student</label>
+        <select
+          name="students" 
+          id='student-select'
+          onChange={(e) => this.handleStudentSelect(e.currentTarget.value)}
+        >
+          <option defaultValue="Select">--make a selection--</option>
+          {this.props.allStudents?.map((student: Student) => {
+            return(
+              <option 
+                value={student.id}
+                key={`SOP${student.id}`}
+                disabled = {this.props.teacher
+                  ? !student.partners.includes(this.props.teacher.id)
+                  : undefined}
+              >
+                {student.displayName}
+              </option>
+            )
+          })}
+        </select>
+      </div>
     )
   }
 
@@ -242,26 +256,37 @@ class ScheduleMeeting extends React.Component<SMProps,SMState>{
   render(){
     console.log(this.props.teacher)
     return(
-      <div className="absolute top-1/4 left-1/4 bg-white text-black border border-gray-500 p-3 shadow-xl">
-        <p className="font-bold text-xl text-blue-500" style={{color:"blue"}}>Schedule Meeting</p>
-        {this.props.teacher && this.props.student
-          ? <p className="font-semibold mt-2">Meet with {this.props.student.displayName}</p>
-          : this.props.student ? this.renderTeacherSelect() : this.renderStudentSelect()
-        }
-        
-        <form onSubmit={(e: React.FormEvent<HTMLFormElement>) => this.handleSubmit(e)}>
-          <label className="font-semibold mt-2" htmlFor="meeting-time">Time </label>
-          <input 
-            className=" mt-2 mb-2" 
-            onChange={this.handleDate} 
-            type="datetime-local" 
-            id="meeting-time"
-            name="meeting-time" 
-            defaultValue={this.getDateString(new Date())}
-            min={this.getDateString(new Date())} 
-            max={this.getDateString(this.getMaxDate())}
-          >
-          </input>
+      <div className = "absolute top-1/4 left-1/4 bg-white text-black border border-gray-500 p-3 shadow-xl"> 
+        <p className="text-center font-bold text-xl text-blue-500 mb-4" style={{color:"blue"}}>Schedule Meeting</p>
+        <form 
+          onSubmit={(e: React.FormEvent<HTMLFormElement>) => this.handleSubmit(e)}
+          className="grid grid-rows-2 grid-cols-1 gap-4"
+        > 
+          <div className="grid grid-cols-2 grid-rows-2 grid-flow-col gap-4">
+            {this.props.teacher && this.props.student
+              ? <p className="font-semibold mt-2">Meet with {this.props.student.displayName}</p>
+              : this.props.student ? this.renderTeacherSelect() : this.renderStudentSelect()
+            }
+            <div>
+              <label className="font-semibold mt-2" htmlFor="meeting-date">Date </label>
+              <input 
+                className=" mt-2 mb-2" 
+                onChange={this.handleDate} 
+                type="date"
+                id="meeting-date"
+                name="meeting-date" 
+                defaultValue={this.getDateString(new Date())}
+                min={this.getDateString(new Date())} 
+                max={this.getDateString(this.getMaxDate())}
+              >
+              </input>
+            </div>
+            <div className="col-span-2">
+              <p>
+                Gonna be picking a time
+              </p>
+            </div>
+          </div>
           <div className="flex flex-row justify-start mt-2">
             <button 
               className="max-h-5 px-2 py-1 flex items-center text-xs uppercase font-bold hover:opacity-75"
@@ -272,7 +297,6 @@ class ScheduleMeeting extends React.Component<SMProps,SMState>{
           </div>
           {/* <input type='submit' value='Submit'/> */}
         </form>
-
       </div>
     )
   }
